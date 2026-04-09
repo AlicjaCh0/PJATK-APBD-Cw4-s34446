0dd635a909a027d0dc769fb378e46f0b320a3a16
Alicja Choda <s34446@pjwstk.edu.pl>
2026-04-09 21:53:50 +0200

    Dodanie interfejsu IBillService i klasy LegacyBillingService wraz z logiką

---
+++ LegacyRenewalApp/IBillService.cs
@@ -0,0 +1,9 @@
+﻿namespace LegacyRenewalApp;
+
+public interface IBillService
+{
+    void SaveInvoice(RenewalInvoice r);
+    
+    void SendNotification(string email, string customerName, string planCode, decimal amount);
+    
+}
---
+++ LegacyRenewalApp/LegacyBillingService.cs
@@ -0,0 +1,20 @@
+﻿namespace LegacyRenewalApp;
+
+public class LegacyBillingService : IBillService
+{
+    
+    
+    public void SendNotification(string customerEmail,string customerName, string planCode, decimal amount)
+    {
+        string tyt = "Faktura za odnowienie subskrybcji";
+        
+        string body = $"Dzien dobry {customerName}, faktura za twoją umowę {planCode} jest gotowa. Wartosc wynosi: {amount:F2}.";
+        LegacyBillingGateway.SendEmail(customerEmail, tyt, body);
+        
+    }
+    
+    
+    public void SaveInvoice(RenewalInvoice invoice) => LegacyBillingGateway.SaveInvoice(invoice);
+    
+    
+}
